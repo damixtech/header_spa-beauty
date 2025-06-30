df15db845c88c042620ddc2640abeb951774a51b
Damian AR <damitech@tutamail.com>
2025-06-30 12:17:50 +0200

    Updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // Rutas relativas para GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/header_spa-beauty/' : '/',
+
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos de producción
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      },
+    },
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
